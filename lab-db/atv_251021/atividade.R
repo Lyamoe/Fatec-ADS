3398c4e54c9e375ace7360a37872ed701c285420
Lyamoe <karinesanper@gmail.com>
2025-10-22 21:26:06 -0300

    atividade lab db elenco times

---
+++ lab-db/atv_251021/atividade.R
@@ -0,0 +1,14 @@
+install.packages("readxl")
+
+library(readxl)
+
+dados_time <- read_excel("elenco_times_sp.xlsx")
+View(dados_time)
+
+minimo <- min(dados_time$Idade)
+maximo <- max(dados_time$Idade)
+media <- mean(dados_time$Idade)
+
+print(minimo)
+print(maximo)
+print(media)
